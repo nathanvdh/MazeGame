62325bee1d986a62236693406715f8224bae17a7
nathanvdh <55475809+nathanvdh@users.noreply.github.com>
2019-10-08 09:40:04 +1030

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -7,26 +7,16 @@ class Entity
 {
 public:
 	Entity();
-	Entity(std::string aName, int aSize[2], char aSprite);
-	
-	void setName(std::string aName);
-	std::string getName();
+	Entity(char aSprite);
 
 	void setSprite(char aSprite);
 	char getSprite();
-
-	void setSize(int aSize[2]);
-	int* getSize();
 	
 	
 	~Entity();
 
 private:
-	std::string name;
 	char sprite;
-	int size[2];
-
-	
 };
 
 #endif
