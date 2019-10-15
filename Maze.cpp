034746d4576991e25a9d17f8055cb97484330ec8
nathanvdh <a1743599@student.adelaide.edu.au>
2019-10-15 21:12:08 +1030

    Added drawmap to maze.h/maze.cpp

--- Maze.cpp
+++ Maze.cpp
@@ -4,8 +4,29 @@ Maze::Maze() {
 
 }
 
-void setmMap(Obstacle* aMap[5,5]) {
-	map = aMap;
+void Maze::setMap(Obstacle* aMap[][5]) {
+	int row, column;
+  	
+  	for (row=0; row < mapHeight; row++ ){
+    	for (column=0; column < mapWidth ;column++) {
+      		map[row][column] = aMap[row][column];
+    	}
+  	}
+}
+
+void Maze::drawMap(){//Obstacle* aMap[5,5], int mapWidth, int mapHeight) {
+	int row,column;
+
+  	for (row=0;row<5;row++ ){
+    	for (column=0;column<5;column++) {
+      		if (myMap[row][column]) {
+      		mvaddch(row,column,myMap[row][column]->getSprite());	
+      		} else {
+      			mvaddch(row,column,' ');
+      		}
+
+    	}
+  	}
 }
 
 Maze::~Maze() {
--- Maze.h
+++ Maze.h
@@ -2,16 +2,18 @@
 #define MAZE_H
 
 #include "Obstacle.h"
-
+#include <curses.h>
 class Maze {
 public:
 	Maze();
-	void setMap(Obstacle* aMap[5,5]);
-	void drawMap(Obstacle* aMap[5,5]);
+	void setMap(Obstacle* aMap[5][5]);
+	void drawMap();
 	~Maze();
 	
 private:
-	Obstacle* map[5,5];
+	Obstacle* map[5][5];
+	int mapWidth;
+	int mapHeight;
 };
 
 #endif //MAZE_H
