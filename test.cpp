a232bea5b9cd4ecf84df0bf0e41901f2fee67a07
harrybag <55475832+harrybag@users.noreply.github.com>
2019-10-15 22:13:29 +1030

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -15,6 +15,7 @@ int main(void)
 	keypad(stdscr, TRUE); //enable function/arrow keys
 	curs_set(0); //disable cursor
 	noecho(); //disable echoing of input
+	int inp = '\0';
 	//Maze *maze;
 	Wall *wall;
 	Finish *finish;
@@ -35,22 +36,10 @@ int main(void)
 	maze.setMap(myMap);
 
 	//initCurses();
-	initscr(); //initialize screen
-	cbreak(); //force cbreak mode (dont need to press enter to put character)
-	keypad(stdscr, TRUE); //enable function/arrow keys
-	curs_set(0); //disable cursor
-	noecho(); //disable echoing of input
-	int inp = '\0';
 
 	maze.drawMap();
-
-	//initialisation of map dimensions
-	const int MAPHEIGHT = 5, MAPWIDTH = 5;
-	//intialisation of position variables
-	int xPos=2, yPos=2;
 	
 	//drawing of map and text
-	drawMap(map,MAPWIDTH, MAPHEIGHT);
 	mvprintw(MAPHEIGHT,1,"Reach the end zone (X) to complete the game");
 	mvprintw(MAPHEIGHT+2,1,"Press q to quit game");
 	
@@ -64,6 +53,7 @@ int main(void)
 	{
 		inp = getch();
 		
+		//Switch statement checks the objects surround the player
 		switch (inp) {
 			case: UP
 				move = map[yPos++][xPos]->touched();
