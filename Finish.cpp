b833b86212314953664c193ea4720116ff00e60e
harrybag <55475832+harrybag@users.noreply.github.com>
2019-10-15 22:34:05 +1030

    Update Finish.cpp

--- Finish.cpp
+++ Finish.cpp
@@ -7,9 +7,7 @@ Finish::Finish() : Obstacle('F', "You reached the finish") {
 }
 
 bool Finish::touched() {
-	
-	std::cout << message << "\n";
-	//exit program?? not sure how
+	mvprintw(8,1, message);
 	return 1;
 }
 
