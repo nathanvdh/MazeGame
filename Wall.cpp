6bc0dcd8b1566b920bc80034c97aff00aed6802c
harrybag <55475832+harrybag@users.noreply.github.com>
2019-10-15 22:19:40 +1030

    Update Wall.cpp

--- Wall.cpp
+++ Wall.cpp
@@ -6,10 +6,11 @@ Wall::Wall() : Obstacle('#', "You hit a wall") {
 
 }
 
-void Wall::touched() {
+bool Wall::touched() {
 	//Put player backward or don't let them move forward (or something like that)
 	//But would that be tight coupling?
 	std::cout << message << "\n";
+	return 0;
 }
 
 Wall::~Wall() {
