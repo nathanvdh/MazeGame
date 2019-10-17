287b5bed2c7d519f3ad3e266db8d8f9142b4b32d
nathanvdh <a1743599@student.adelaide.edu.au>
2019-10-17 18:36:46 +1030

    Update messages to include extra spaces to overwrite pervious messages

--- Hole.cpp
+++ Hole.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 #include <string>
 
-Hole::Hole() : Obstacle('O', "You fell in a hole") {
+Hole::Hole() : Obstacle('O', "You fell in a hole                      ") {
 
 }
 
 bool Hole::touched(Maze* maze, Person* person, int keyPress) {
 	mvaddch(person->getyPos(),person->getxPos(),maze->getMap()[person->getyPos()][person->getxPos()]->getSprite());
-	person->setPos(1,1);
+	person->setPos(2,2);
 	mvprintw(maze->getMapHeight(),0, message.c_str());
 
 	return 0;
