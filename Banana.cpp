454fcb72e55a14e8304d34838977d05b524b90e7
nathanvdh <a1743599@student.adelaide.edu.au>
2019-10-17 18:31:31 +1030

    Fix banana implementation

--- Banana.cpp
+++ Banana.cpp
@@ -2,18 +2,40 @@
 #include <iostream>
 #include <string>
 
-Banana::Banana() : Obstacle('.', "You slipped on a Banana") {
+
+Banana::Banana() : Obstacle('(', "You slipped on a banana                        ") {
 
 }
 
 bool Banana::touched(Maze* maze, Person* person, int keyPress) {
 	mvprintw(maze->getMapHeight(),0, message.c_str());
 	//moves person onto and off banana
-	person->move(keyPress,maze->getMap()[person->getyPos()][person->getxPos()]->getSprite());
-	person->move(keyPress,maze->getMap()[person->getyPos()][person->getxPos()]->getSprite());
+	movePerson(maze, person, keyPress);	
+	//person->move(keyPress,maze->getMap()[person->getyPos()][person->getxPos()]->getSprite());
 	//continually moves person
-	while(maze->getMap()[person->getyPos()][person->getxPos()]->getSprite() == (' ')) {
-		person->move(keyPress,maze->getMap()[person->getyPos()][person->getxPos()]->getSprite());
+	
+	switch (keyPress) {
+		case KEY_UP:
+			while(maze->getMap()[person->getyPos()-1][person->getxPos()]->getSprite() == (' ')) {
+				movePerson(maze, person, keyPress);	
+			}
+			break;
+		case KEY_DOWN:
+			while(maze->getMap()[person->getyPos()+1][person->getxPos()]->getSprite() == (' ')) {
+				movePerson(maze, person, keyPress);	
+			}
+			break;
+		case KEY_LEFT:
+			while(maze->getMap()[person->getyPos()][person->getxPos()-1]->getSprite() == (' ')) {
+				movePerson(maze, person, keyPress);	
+			}
+			break;
+		case KEY_RIGHT:
+			while(maze->getMap()[person->getyPos()][person->getxPos()+1]->getSprite() == (' ')) {
+				movePerson(maze, person, keyPress);	
+			}
+			break;
+
 	}
 	return 0;
 }
