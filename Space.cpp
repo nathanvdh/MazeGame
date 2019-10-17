f84cb41e82777c53179bfc57c5516439953b51dc
nathanvdh <a1743599@student.adelaide.edu.au>
2019-10-17 21:55:33 +1030

    Change space message to spaces (to clear)

--- Space.cpp
+++ Space.cpp
@@ -2,13 +2,16 @@
 #include <iostream>
 #include <string>
 
-Space::Space() : Obstacle(' ', "\0") {
+Space::Space() : Obstacle(' ', "                                       ") {
 
 }
 
 bool Space::touched(Maze* maze, Person* person, int keyPress) {
-	movePerson(maze, person, keyPress);	
+	movePerson(maze, person, keyPress);
 	mvprintw(maze->getMapHeight(), 0, message.c_str());
+		
+			
+	
 	return 0;
 }
 
