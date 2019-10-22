987da42465e60786804620c4f070ac0338c37e08
nathanvdh <a1743599@student.adelaide.edu.au>
2019-10-22 22:44:29 +1030

    cleaning

--- Banana.cpp
+++ Banana.cpp
@@ -16,10 +16,7 @@ bool Banana::touched(Maze* maze, Person* person, int keyPress) {
 		 }
 		nextOb = maze->getNextObstacle(person, keyPress); 	//get the next obstacle relative to the new position
 	}
-
 	
-
-
 	Obstacle::touched(maze, person, keyPress);
 	return 1; //person was moved involuntarily so touched returns 1
 }
--- Finish.cpp
+++ Finish.cpp
@@ -10,7 +10,6 @@ Finish::Finish() : Obstacle('X', "You reached the finish, press any key to exit 
 bool Finish::touched(Maze* maze, Person* person, int keyPress) {
 	Obstacle::touched(maze, person, keyPress);
 	person->drawPerson();
-	//clear();
 	getch();
 	endwin();
 	exit(0);
