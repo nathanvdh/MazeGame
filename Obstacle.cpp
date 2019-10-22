1bd7e6a4d696e8c4bbb65fe56d233be93e899a31
nathanvdh <a1743599@student.adelaide.edu.au>
2019-10-22 23:02:26 +1030

    Comment

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -7,7 +7,7 @@ Obstacle::Obstacle(char aSprite, std::string aMessage) : Entity (aSprite) {
 }
 
 bool Obstacle::touched(Maze* maze, Person* person, int keyPress) {
-	mvprintw(maze->getMapHeight(),0, message.c_str());
+	mvprintw(maze->getMapHeight(),0, message.c_str()); //print message below map
 	return 0;
 }
 
